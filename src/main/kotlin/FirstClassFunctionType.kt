93b1f92e3573806c19e95c95462a2ba7385d8602
Gyeongjun Kim <lastpuzly@gmail.com>
2021-02-19 15:15:14 +0900

    feat: implement add()

--- src/main/kotlin/FirstClassFunctionType.kt
+++ src/main/kotlin/FirstClassFunctionType.kt
@@ -4,8 +4,12 @@ typealias Converter<T> = (T) -> T
 
 fun <T> convert(value: T, converter: Converter<T>): T = converter(value)
 
+fun <T> add(a: T, b: T, op: (T, T) -> T): T = op(a, b)
+
 fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
     val value = input.nextDouble()
     println(convert(value) { it * 10 })
+
+    val result = add(10, 20) { a, b -> a + b }
 }
